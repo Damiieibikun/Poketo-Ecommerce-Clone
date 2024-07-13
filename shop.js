d775c3b3fac92bd0f4b77a85ef26b1ef73cd1e46
Damilola Ibikunle <damiieibikun@gmail.com>
2024-07-13 15:26:59 +0100

    added shop page with products

---
+++ shop.js
@@ -0,0 +1,73 @@
+$(document).ready(() => {
+
+    let products = JSON.parse(localStorage.getItem('Poketo-Products')) || []
+    products.forEach((product) => {
+        let showStars = 'flex'
+        if (product.quantity === null) {
+            showStars = 'none'
+        }
+
+        let productItem = $(`<div class="d-grid">
+                <div class="d-slider-product-item d-flex" id="d-slider-product${product.id}" style="background-image: url(${product.img})" onMouseOver="this.style.backgroundImage='url(${product.imgHover})'" onMouseOut="this.style.backgroundImage='url(${product.img})'">
+                    <div class="d-item-tag" style="background-color: ${product.tagColor}">${product.tag}</div>
+                    <button class="d-addCart d-display-none">Add to Cart</button>
+                </div>
+                <div class="d-slider-product-desc">
+                    <p class="d-product-title-slider">${product.productName}</p>
+                    <div class="d-flex d-justify-between d-align-center">
+                        <div class="star" style = "display: ${showStars}">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path></svg>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z"
+                                fill="#dedede"></path>
+                            </svg>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path></svg>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z"
+                                    fill="#dedede"></path>
+                                </svg>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.99998 13.0852L3.28889 15.4762C3.18255 15.5302 3.05189 15.4891 2.99706 15.3844C2.97577 15.3438 2.96832 15.2975 2.97581 15.2523L3.83017 10.103L0.064192 6.43136C-0.0208179 6.34848 -0.0214783 6.21346 0.062717 6.12978C0.0954128 6.09728 0.137856 6.07599 0.183781 6.06906L5.4229 5.27766L7.80648 0.617401C7.86029 0.512205 7.99054 0.469862 8.09741 0.522826C8.13891 0.543393 8.17259 0.57655 8.19349 0.617401L10.5771 5.27766L15.8162 6.06906C15.9345 6.08692 16.0156 6.19578 15.9975 6.31219C15.9904 6.3574 15.9688 6.39918 15.9358 6.43136L12.1698 10.103L13.0242 15.2523C13.0434 15.3686 12.9634 15.4782 12.8453 15.4972C12.7994 15.5045 12.7524 15.4972 12.7111 15.4762L7.99998 13.0852Z" fill="#dedede"></path></svg>
+                            <span class="d-slider-product-rating">${product.stars}(${product.quantity})</span>
+                        </div>
+                        <p class="d-slider-product-price">$${product.price}</p>
+                    </div>
+                    
+                    <div class="d-product-colors d-flex d-gap-10">
+
+                    </div>
+                </div>
+            </div>`)
+
+
+        let productRating = productItem.find('.star').find('svg')
+        productRating.each((i, svg) => {
+            $(svg).find('path').css('fill', '#ef4043')
+            if (i === Math.round(product.stars) - 1) {
+                return false;
+            }
+
+        })
+        if ('availableImage' in product) {
+            product.availableImage.forEach((img, i) => {
+                productItem.find('.d-product-colors').append(`<div class="d-color-selection-outer">
+                            <div class="d-color-selection" data-id=${i}" style="background-image: url(${img}); background-color: transparent"></div>
+                        </div>`)
+            })
+        }
+        if ('availableColors' in product) {
+            product.availableColors.forEach((color, i) => {
+                productItem.find('.d-product-colors').append(`<div class="d-color-selection-outer">
+                            <div class="d-color-selection" data-id=${i}" style="background-image: url(''); background-color: ${color}"></div>
+                        </div>`)
+            })
+        }
+        $('#d-shopAll-grid-items').append(productItem)
+    })
+    $('.d-slider-product-item').mouseover(function() {
+        $(this).find('.d-addCart').show()
+    })
+
+    $('.d-slider-product-item').mouseout(function() {
+        $(this).find('.d-addCart').hide()
+    })
+})
